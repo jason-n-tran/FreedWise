3957cadae058a6a28905e6af12f9542bbef510ca
jason-n-tran <thismart595@gmail.com>
2026-09-06 16:13:21 -0500

    add index component

---
+++ src/components/index.ts
@@ -0,0 +1,11 @@
+// Component exports
+
+export { default as Button } from './common/Button';
+export { default as MarkerText } from './common/MarkerText';
+export { default as Masthead } from './common/Masthead';
+export { default as ErrorBoundary } from './ErrorBoundary';
+export { default as TextSelectionMenu } from './TextSelectionMenu';
+export { default as HighlightModal } from './HighlightModal';
+export { default as GradingButtons } from './GradingButtons';
+export { default as ReviewSession } from './ReviewSession';
+export type { ReviewHighlight } from './ReviewSession';
